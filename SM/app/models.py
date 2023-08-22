f26aa69644816b9cba6f5b49522e8ecefa499a4a
otman lahreche <otman.lahreche@gmail.com>
2023-08-22 16:27:53 +0100

    update

--- SM/app/models.py
+++ SM/app/models.py
@@ -2,67 +2,25 @@ from django.db import models
 
 
 
-class Service(models.Model):
+class Exemen(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
+    prenom = models.CharField(max_length=255)
+    date_naissance = models.DateField()
+    date_test = models.DateField()
+    no_registre = models.IntegerField()
+    HIV_test = models.CharField(max_length=150)
+    HBS_test = models.CharField(max_length=150)
+    HCV_test = models.CharField(max_length=150)
+    BW_test = models.CharField(max_length=150)
+    TOXOPLASME_test = models.CharField(max_length=150)
+    RUBIOLE_test = models.CharField(max_length=150)
+    observation = models.CharField(max_length=500)
+    patient_genre = models.CharField(max_length=150)
 
     def __str__(self):
-        return self.name
+        return self.id
     
-class TypeArticle(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-
-
-    def __str__(self):
-            return self.name
-    
-
-class Article(models.Model):
-     id = models.AutoField(primary_key=True)
-     name = models.CharField(max_length=100)
-     type_article = models.ForeignKey(TypeArticle, on_delete=models.CASCADE)
-     observation = models.CharField(max_length=100)
-
-     def __str__(self):
-            return self.name
-     
-
-
-class Stock(models.Model):
-     id = models.AutoField(primary_key=True)
-     stock_number = bon_number = models.IntegerField()
-     article = models.ForeignKey(Article, on_delete=models.CASCADE)
-     date_arrived = models.DateField()
-     stock_qte = models.IntegerField()
-
-     def __str__(self):
-            return self.stock_number
-
-
-class Sortie(models.Model):
-     id = models.AutoField(primary_key=True)
-     bon_number = models.IntegerField()
-     service = models.ForeignKey(Service, on_delete=models.CASCADE)
-     stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
-     date_sortie = models.DateField()
-     sortie_qte = models.IntegerField()
-
-     def __str__(self):
-            return self.stock_number
-     
-
-class History(models.Model):
-      id = models.AutoField(primary_key=True)
-      type_operation = models.CharField(max_length=50)
-      number = models.IntegerField()
-      service = models.CharField(max_length=100)
-      article = models.CharField(max_length=100)
-      type_article = models.CharField(max_length=100)
-      qte = models.IntegerField()
-
-      def __str__(self):
-            return self.stock_number
       
 
 
--- SM/app/views.py
+++ SM/app/views.py
@@ -1,3 +1,84 @@
 from django.shortcuts import render
-
+import datetime
+from os import stat
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.response import Response
+from calendar import monthrange
+from .models import *
+from .serializers import *
+from rest_framework import status
 # Create your views here.
+
+
+@api_view(['GET'])
+def getAllExamenOfYear(request, year):
+    if request.method == 'GET' and request.user.is_authenticated:
+
+        range = monthrange(year, 12)
+
+        date_start = datetime.date(year , 1, 1)
+        date_end = datetime.date( year, 12, range[1])
+
+        queryset = Exemen.objects.filter(date_test__gte=date_start, date_test__lte=date_end).order_by("-date_test")
+
+        source_serial = ExamenSerializer(queryset, many=True)
+
+        return Response(status=status.HTTP_200_OK,data=source_serial.data)
+                
+    else :
+        return Response(status=status.HTTP_401_UNAUTHORIZED) 
+    
+
+@api_view(['POST'])
+def createNewExemen(request):
+    if request.method == 'POST' and request.user.is_authenticated:
+
+        name = request.data.pop('name')
+        prenom = request.data.pop('prenom')
+        date_n = request.data.pop('date_naissance')
+        date_t = request.data.pop('date_test')
+        no_registre = request.data.pop('no_registre')
+        patient_genre = request.data.pop('patient_genre')
+        HIV_test = request.data.pop('HIV_test')
+        HBS_test = request.data.pop('HBS_test')
+        HCV_test = request.data.pop('HCV_test')
+        BW_test = request.data.pop('BW_test')
+        TOXOPLASME_test = request.data.pop('TOXOPLASME_test')
+        RUBIOLE_test = request.data.pop('RUBIOLE_test')
+        observation = request.data.pop('observation')
+
+        date_naissance = date_n.split("/")
+        date_test = date_t.split("/")
+
+        d_n = date_naissance[0]
+        m_n = date_naissance[1]
+
+        d_t = date_test[0]
+        m_t = date_test[1]
+
+        if d_n[0] == '0':
+            d_n.replace('0','',1)
+        if m_n[0] == '0':
+            m_n.replace('0','',1)
+        if d_t[0] == '0':
+            d_t.replace('0','',1)
+        if m_t[0] == '0':
+            m_t.replace('0','',1)
+
+        date_naissance[0] = d_n
+        date_naissance[1] = m_n
+        date_test[0] = d_t
+        date_test[1] = m_t
+
+        
+        date_naissance = datetime.date(int(date_naissance[2]), int(date_naissance[1]), int(date_naissance[0]))
+        date_test = datetime.date(int(date_test[2]), int(date_test[1]), int(date_test[0]))
+
+
+        source = Exemen.objects.create(name = name, prenom = prenom, date_naissance=date_naissance, date_test=date_test, no_registre=no_registre, HIV_test=HIV_test, HBS_test=HBS_test, HCV_test=HCV_test, BW_test=BW_test, TOXOPLASME_test=TOXOPLASME_test, RUBIOLE_test=RUBIOLE_test, observation=observation, patient_genre=patient_genre)
+
+        if source.id is not None:
+            return Response(status=status.HTTP_201_CREATED, data = {"id_examen":source.id})
+        else:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
